3624bc26c7ff5e843bd86e9ea952407cc13979e6
Bryan Carney <bryancarney1@gmail.com>
2024-12-28 15:09:50 -0500

    Add files via upload

---
+++ data_formatting.sql
@@ -0,0 +1,28 @@
+
+CREATE TABLE employees_temp (
+    emp_no INTEGER PRIMARY KEY,
+    emp_title_id CHAR(5) NOT NULL,
+    birth_date TEXT, 
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20),
+    sex CHAR(1) NOT NULL,
+    hire_date TEXT NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+INSERT INTO employees (emp_no, emp_title_id, birth_date, first_name, last_name,sex,hire_date)
+SELECT 
+	et.emp_no, 
+	et.emp_title_id, 
+	TO_DATE(et.birth_date, 'MM-DD-YYYY'),
+	et.first_name, 
+	et.last_name,
+	et.sex,
+	TO_DATE(et.hire_date, 'MM-DD-YYYY')
+FROM employees_temp AS et;
+
+
+SELECT * FROM employees;
+
+DROP TABLE employees_temp;
+
---
+++ data_tables_schema.sql
@@ -0,0 +1,43 @@
+CREATE TABLE departments (
+    dept_no CHAR(4) PRIMARY KEY,
+    dept_name VARCHAR(100)
+);
+
+CREATE TABLE titles (
+    title_id CHAR(5) PRIMARY KEY,
+    title VARCHAR(20) NOT NULL
+);
+
+--intial import of data was unsuccessful, once date parameters were reformatted the file was able to be loaded successfully
+CREATE TABLE employees (
+    emp_no INTEGER PRIMARY KEY,
+    emp_title_id CHAR(5) NOT NULL,
+    birth_date DATE, 
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20),
+    sex CHAR(1) NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE dept_emp (
+    emp_no INTEGER NOT NULL,
+    dept_no CHAR(4) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no CHAR(4) NOT NULL,
+    emp_no INTEGER NOT NULL,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE salaries (
+    emp_no INTEGER NOT NULL PRIMARY KEY, 
+    salary INTEGER DEFAULT 0 CHECK (salary >= 0), 
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
